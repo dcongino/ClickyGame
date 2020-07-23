4358fc640e21d1c33ca13db60715d6d54752fb6f
Drew Congino <dcongino@yahoo.com>
2020-07-23 11:58:06 -0600

    Updates

---
+++ precache-manifest.45ca380273bbf4b4550212c2c95b9a7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0990f6803f22075f1f241a99d7cf9610",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "304594fa9253785a24ee",
+    "url": "/ClickyGame/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9374fbafa062dff26bdc",
+    "url": "/ClickyGame/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "304594fa9253785a24ee",
+    "url": "/ClickyGame/static/js/2.8ec9f5a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ClickyGame/static/js/2.8ec9f5a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9374fbafa062dff26bdc",
+    "url": "/ClickyGame/static/js/main.e70233fc.chunk.js"
+  },
+  {
+    "revision": "29cfca7e2c10f6cba413",
+    "url": "/ClickyGame/static/js/runtime-main.5e5c4f3b.js"
+  }
+]);
